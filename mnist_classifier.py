f4762a3a10893ea278c4bf884753939b81678167
An-Khiem Le <ankhiemle02@gmail.com>
2025-05-28 17:35:15 -0400

    updated model training

--- mnist_classifier.py
+++ mnist_classifier.py
@@ -2,7 +2,7 @@
 # Project 1 - MNIST Classifier
 # Students: 
     # - Sopheaktra Lean 40225014
-    # -
+    # - An-Khiem Le 40280775
     # -
 # Deadline: June 1, 2025
 
@@ -10,7 +10,9 @@
 import numpy as np                     
 import matplotlib.pyplot as plt       
 from sklearn.datasets import load_digits  
-from sklearn.model_selection import train_test_split  
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 # Load the MNIST dataset
 digits = load_digits()  
@@ -42,8 +44,16 @@ print("Testing set size:", X_test.shape[0])
 
 # Model Training
 
+# Set max_iter to 1000. This sets the max number of iterations the modeil will go through to converge.
+model = LogisticRegression(max_iter=1000) 
+model.fit(X_train, y_train)
 
+# Making predictions on test data
+y_predict = model.predict(X_test)
 
+# Compariung the predicted labels with the actual label
+score = accuracy_score(y_predict, y_test)
+print('Logistic Regression MNIST dataset: {}%'.format(score * 100))
 
 
 # Model Evaluation
