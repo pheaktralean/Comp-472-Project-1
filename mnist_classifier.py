d02dbc34749d9a31923fb4de5d33771b3b458023
Axe <175651147+Axe-Kaj@users.noreply.github.com>
2025-05-29 22:08:49 -0400

    Updated libary sklearn metrics to 1 line

--- mnist_classifier.py
+++ mnist_classifier.py
@@ -12,8 +12,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits  
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import seaborn as sn
 
 ## Load the MNIST dataset
